3befa242aa9f010412f4a8967c54d509ecc11c79
Javad Fazli <javadfazli2000@gmail.com>
2025-01-13 18:37:55 +0330

    admin can add film

--- filmers/films/admin.py
+++ filmers/films/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Film, Review
 
-# Register your models here.
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ('name', 'genre', 'release_year', 'get_average_rating')
+    list_filter = ('genre', 'release_year')
+    search_fields = ('name', 'genre')
+    ordering = ('-release_year',)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('film', 'user', 'rating', 'comment')
+    list_filter = ('rating', 'film')
+    search_fields = ('film__name', 'user', 'comment')
